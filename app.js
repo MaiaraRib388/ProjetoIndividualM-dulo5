111237d387a33e0c7f33f15c16310d81ab400b8a
MaiaraRib388 <113558275+MaiaraRib388@users.noreply.github.com>
2023-02-15 16:16:40 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const redlineSync = require("readline-sync");
+
+const lista = [];
+let resp = "";
+
+const adicionarNaLista = (prop) => {
+  lista.push(prop);
+};
+
+const ordenar = () => {
+  lista.sort();
+};
+
+const listar = () => {
+  lista.forEach((elemento)=> { console.log(elemento); }); //Lista por linha como string
+};
+
+while (true) {
+  resp = redlineSync.question("Digite a propriedade CSS ou 'SAIR' para exibir a listagem e encerrar: ");
+
+  if (resp.toLowerCase() == "sair") {
+    ordenar();
+    listar();
+  break;
+ 
+  } else if (resp.toLowerCase() !== "sair" && resp !=="") {
+    adicionarNaLista(resp);
+  }
+}
